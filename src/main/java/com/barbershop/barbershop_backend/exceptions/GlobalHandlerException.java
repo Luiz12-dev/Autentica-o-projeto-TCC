6bddc730aa5607f76da3a47eee063a5422250804
Luiz12-dev <aluizotaviodealmeida@gmail.com>
2025-11-19 17:07:03 -0300

    feat(Exception): Adiciona dois gerenciadores de exceções, MethodArgumentNotValidException e AccessDeniedException

--- src/main/java/com/barbershop/barbershop_backend/exceptions/GlobalHandlerException.java
+++ src/main/java/com/barbershop/barbershop_backend/exceptions/GlobalHandlerException.java
@@ -1,13 +1,38 @@
 package com.barbershop.barbershop_backend.exceptions;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalHandlerException {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex){
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(),
+         error.getDefaultMessage()));
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(DuplicatedEmailException.class)
     public String handleDuplicatedEmailException(DuplicatedEmailException ex){
         return ex.getMessage();
     }  
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<Map<String, String>> handleAccessDeniedException(AccessDeniedException ex){
+        return new ResponseEntity<>(Map.of("error", "Access Denied", "message", "You do not have permission to access this resource."),
+         HttpStatus.FORBIDDEN);
+    }
+ 
+
 }    
