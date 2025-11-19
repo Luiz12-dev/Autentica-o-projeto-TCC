734341dbaf090898d0c195662b45fd292492e192
Luiz12-dev <aluizotaviodealmeida@gmail.com>
2025-11-19 16:37:59 -0300

    refactor(Exception): Corrige anotação da classe.

--- src/main/java/com/barbershop/barbershop_backend/exceptions/GlobalHandlerException.java
+++ src/main/java/com/barbershop/barbershop_backend/exceptions/GlobalHandlerException.java
@@ -1,9 +1,9 @@
 package com.barbershop.barbershop_backend.exceptions;
 
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestController
+@RestControllerAdvice
 public class GlobalHandlerException {
 
     @ExceptionHandler(DuplicatedEmailException.class)
